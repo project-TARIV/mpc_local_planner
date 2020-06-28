b34ba45ebcdb15d88f685458c916626509ecc98d
Suraj Rathi <suraj.rathi00@gmail.com>
2020-06-28 16:59:00 +0530

    Added boilerplate

--- include/mpc_local_planner/mpc_local_planner.h
+++ include/mpc_local_planner/mpc_local_planner.h
@@ -6,6 +6,21 @@
 
 namespace mpc_local_planner {
     class MPC_Local_Planner : public nav_core::BaseLocalPlanner {
+    public:
+        void initialize(std::string name, tf2_ros::Buffer *tf, costmap_2d::Costmap2DROS *costmap_ros);
+
+        bool setPlan(const std::vector<geometry_msgs::PoseStamped> &plan);
+        
+        bool computeVelocityCommands(geometry_msgs::Twist &cmd_vel);
+
+        bool isGoalReached();
+
+        ~MPC_Local_Planner();
+
+    //protected:
+        MPC_Local_Planner();
+
+    private:
 
     };
 }
--- src/mpc_local_planner.cpp
+++ src/mpc_local_planner.cpp
@@ -1,3 +1,6 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
 #include "../include/mpc_local_planner/mpc_local_planner.h"
 //#include "mpc_local_planner/mpc_local_planner.h"
 
@@ -8,3 +11,31 @@ PLUGINLIB_EXPORT_CLASS(mpc_local_planner::MPC_Local_Planner, nav_core::BaseLocal
 
 // implement MPC_Local_Planner
 using namespace mpc_local_planner;
+
+MPC_Local_Planner::MPC_Local_Planner() {
+    ROS_INFO("Constructing Planner");
+}
+
+MPC_Local_Planner::~MPC_Local_Planner() {
+    ROS_INFO("Destroying Planner");
+}
+
+
+void MPC_Local_Planner::initialize(std::string name, tf2_ros::Buffer *tf, costmap_2d::Costmap2DROS *costmap_ros) {
+    ROS_INFO("Initing Planner");
+}
+
+bool MPC_Local_Planner::setPlan(const std::vector<geometry_msgs::PoseStamped> &plan) {
+    ROS_INFO("Setting Plan");
+}
+
+bool MPC_Local_Planner::computeVelocityCommands(geometry_msgs::Twist &cmd_vel) {
+    ROS_INFO("Compute vel");
+
+}
+
+bool MPC_Local_Planner::isGoalReached() {
+    ROS_INFO("Checking if goal reached");
+}
+
+#pragma clang diagnostic pop
