15c9ecfc5e3b0fcac8ebe684a3cbe19c603574e2
Suraj Rathi <suraj.rathi00@gmail.com>
2020-07-22 23:56:43 +0530

    small comment

--- src/shadow_casting.h
+++ src/shadow_casting.h
@@ -6,9 +6,6 @@
 using IsBlocked = std::function<bool(int, int)>;
 using AddPoint = std::function<void(int, int)>;
 
-//using IsBlocked = bool (*)(int, int);
-//using AddPoint = void (*)(int, int)>;
-
 void shadow_cast(const IsBlocked &is_blocked, const AddPoint &add_point, const int max_index, const double del = 1e-5);
 
 #endif //MPC_LOCAL_PLANNER_SHADOW_CASTING_H
